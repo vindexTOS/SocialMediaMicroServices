623ebf48069cae9cbc60b9571cd3b24933ed2477
vindexTOS <giorgikutateladze1998@gmail.com>
2024-09-03 17:18:43 +0400

    setting up nest api gateway

--- api-gateway/src/app.module.ts
+++ api-gateway/src/app.module.ts
@@ -1,10 +1,13 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { HttpModule } from '@nestjs/axios';
+import { LaravelControllerController } from './modules/laravel-module/laravel-controller/laravel-controller.controller';
+import { LaravelServiceService } from './modules/laravel-module/laravel-service/laravel-service.service';
 
 @Module({
-  imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [HttpModule],
+  controllers: [AppController, LaravelControllerController],
+  providers: [AppService,   LaravelServiceService],
 })
 export class AppModule {}
